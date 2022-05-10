0006f1da96f91f94a19e6143d338142203351c64
3hui112 <shinetheworld112@gmail.com>
2022-05-10 18:50:14 +0900

    create project; create application.yml

---
+++ src/main/java/com/example/ex_board/ExBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExBoardApplication.class, args);
+    }
+
+}
